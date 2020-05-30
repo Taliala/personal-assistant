57963dfcb04a52c65bdb1d675518b481a89e17ca
pascok <kylepasco@gmail.com>
2020-05-30 15:14:24 -0700

    Add events table

--- personal-assistant-db/personal-assistant-db/dbo/Tables/Events.sql
+++ personal-assistant-db/personal-assistant-db/dbo/Tables/Events.sql
@@ -1,5 +1,27 @@
 ﻿CREATE TABLE [dbo].[Events] (
-    [Id] INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    [EventId]    INT         NOT NULL IDENTITY(1,1),
+	[Id]	UNIQUEIDENTIFIER DEFAULT NEWID(),
+    PRIMARY KEY NONCLUSTERED ([Id] ASC),
+    [Title] NVARCHAR (128) NULL,
+    [Date] DATETIME2 NOT NULL,
+    [Description] NVARCHAR(1024) NULL, 
+    [UserId] UNIQUEIDENTIFIER NULL, 
+    [GroupId] UNIQUEIDENTIFIER NULL, 
+    [CreatedAt] DATETIME2(3) NOT NULL DEFAULT SYSDATETIME(), 
+    [ModifiedAt] DATETIME2(3) NOT NULL DEFAULT SYSDATETIME(), 
+    [CreatedBy] UNIQUEIDENTIFIER NOT NULL, 
+    [ModifiedBy] UNIQUEIDENTIFIER NOT NULL, 
+    [IsActive] BIT NOT NULL DEFAULT 1
+    
 );
+GO;
+CREATE CLUSTERED INDEX IX_EVENTS_EVENTID ON [dbo].[Events] ([EventId] ASC);
+GO;
+CREATE TRIGGER TR_EVENTS_UPDATE on [dbo].[Events] AFTER UPDATE AS
+    UPDATE [dbo].[Events]
+    SET ModifiedAt = SYSDATETIME()
+    FROM Inserted i
+    where [dbo].[Events].Id = i.Id
+GO;
 
+-- TODO Add FK for UserId and GroupId upon completion of those tables
